83b8e1af2d9e8bdb3ca44c2fb9c269259cbeb0d9
christiannaira <christianmnaira@gmail.com>
2024-03-13 10:51:58 +0800

    feat: Implemented & added fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [advice, setAdvice] = useState('');
+  const [id, setId] = useState(null);
+
+  const fetchRandomAdviceApi = () => {
+
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+
+        const adviceData = response.data.slip.advice;
+        const adviceId = response.data.slip.id;
+
+        setAdvice(adviceData);
+        setId(adviceId);
+
+      })
+      .catch((error) => {
+
+        console.log(error);
+
+      })
+
+  }
+
+  fetchRandomAdviceApi();
 
 
   return (
     <>
       <h1>HELLO WORLD</h1>
+      <button>Remind me</button>
+      {advice}
+
     </>
   )
 }
